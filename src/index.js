bcc8088e70f18cb1462277683398c2bae0d3966b
CDBDX\jeremy.zaire <jeremy.zaire@cdiscount.com>
2022-03-07 14:48:12 +0100

    feat: add button and print feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import _ from 'lodash';
+import _ from "lodash";
 import "./style.css";
 import {version} from "../package.json";
 import Dices from "./dices.png";
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'Jérémy', `v${version}`], ' ');
@@ -15,6 +17,12 @@ function component() {
     dices.src = Dices;
 
     element.appendChild(dices);
+
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
 
 
     return element;
